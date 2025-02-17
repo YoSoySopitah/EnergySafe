7aafc6349e66aabba851c92866786b534fc296cf
YoSoySopitah <fandesopitahh@gmail.com>
2025-02-17 08:21:46 -0600

    Initial commit: Estructura básica del proyecto EnergySafe

--- EnergySafe/EnergySafe/settings.py
+++ EnergySafe/EnergySafe/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Application definition
 
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
